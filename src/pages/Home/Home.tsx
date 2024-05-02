c2df244bdbe4f9e6e9e0cd87754f929fab163a31
Sébastien Drouot <contact@seb2dev.fr>
2024-05-02 23:43:53 +0200

    FIX => image path

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,18 +16,15 @@ export const Home = () => {
                 <title>Sync Club | Get early access</title>
                 <meta name="description" content="Join the members-only app that rewards you for paying your bills on time. Track your credit score and build good credit habits." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                {/* Open Graph / Facebook */}
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Sync Club | Get early access" />
                 <meta property="og:description" content="Join the members-only app that rewards you for paying your bills on time. Track your credit score and build good credit habits." />
-                <meta property="og:image" content="https://sync.club/icons/icon-512x512.png" />
-                {/* Twitter */}
+                <meta property="og:image" content="%PUBLIC_URL%/icons/icon-512x512.png" />
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:title" content="Sync Club | Get early access" />
                 <meta property="twitter:description" content="Join the members-only app that rewards you for paying your bills on time. Track your credit score and build good credit habits." />
-                <meta property="twitter:image" content="https://sync.club/icons/icon-512x512.png" />
+                <meta property="twitter:image" content="%PUBLIC_URL%/icons/icon-512x512.png" />
             </Helmet>
-            {/* {component} */}
             <TestB></TestB>
             {/* <TestA></TestA> */}
         </div>
